8933f05bc5fb855bb1f5a533c4dc8616b7c289fb
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-03 22:12:31 -0400

    add basic container and site title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,12 @@
-import logo from "assets/logo.svg";
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 export function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <nav>
+        <h1>Jonathan Chaffer</h1>
+      </nav>
+    </Container>
   );
 }
