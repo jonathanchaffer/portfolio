7bdc051a34314a5af0b5c4d28f1e04b2030b7dc7
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-03 22:46:14 -0400

    add styles and routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export function App(): JSX.Element {
   return (
-    <Container>
-      <nav>
-        <h1>Jonathan Chaffer</h1>
-      </nav>
-    </Container>
+    <Router>
+      <Container>
+        <nav>
+          <Link to="/">
+            <h1>Jonathan Chaffer</h1>
+          </Link>
+          <div>
+            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </nav>
+      </Container>
+    </Router>
   );
 }
