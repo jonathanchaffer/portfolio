709fda30a452fe664a6df7b0c94be42fc5aaa827
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-04 17:56:00 -0400

    add navbar toggler & tagline

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,28 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import "./Navigation.scss";
 
 export function Navigation(): JSX.Element {
   return (
-    <nav>
-      <Link to="/" className="brand">
-        <h1>Jonathan Chaffer</h1>
-      </Link>
-      <div>
+    <Navbar expand="md">
+      <div className="top">
+        <div className="brand">
+          <Link to="/">
+            <h1>Jonathan Chaffer</h1>
+          </Link>
+          <span className="caption d-block d-sm-none">Developer • Designer</span>
+          <span className="caption d-none d-sm-block">Front-End Developer • Graphic Designer</span>
+        </div>
+        <Navbar.Toggle>
+          <i className="fas fa-bars" />
+        </Navbar.Toggle>
+      </div>
+      <Navbar.Collapse>
         <Link to="/work">Work</Link>
         <Link to="/resume">Resume</Link>
         <Link to="/contact">Contact</Link>
-      </div>
-    </nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
