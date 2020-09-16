e1ad818641f9b1a04d939d9ac4d8c2f2fcb1e296
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-16 11:17:08 -0400

    sort works by timestamp

--- src/services/firestoreService.ts
+++ src/services/firestoreService.ts
@@ -2,11 +2,11 @@ import { db } from "index";
 import { DesignWork, DevelopmentWork } from "models";
 
 export async function getDesignWorks(): Promise<DesignWork[]> {
-  const snapshot = await db.collection("designWorks").get();
+  const snapshot = await db.collection("designWorks").orderBy("timestamp", "desc").get();
   return snapshot.docs.map(doc => doc.data() as DesignWork);
 }
 
 export async function getDevelopmentWorks(): Promise<DevelopmentWork[]> {
-  const snapshot = await db.collection("developmentWorks").get();
+  const snapshot = await db.collection("developmentWorks").orderBy("timestamp", "desc").get();
   return snapshot.docs.map(doc => doc.data() as DevelopmentWork);
 }
