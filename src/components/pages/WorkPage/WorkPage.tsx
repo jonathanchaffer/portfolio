34a95bbe9e3f31b310134f7ab0b1602a5eac76b9
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-16 10:44:35 -0400

    remove headings from work page

--- src/components/pages/WorkPage/WorkPage.tsx
+++ src/components/pages/WorkPage/WorkPage.tsx
@@ -4,9 +4,7 @@ import React from "react";
 export function WorkPage(): JSX.Element {
   return (
     <>
-      <h2>Development</h2>
       <DevelopmentSection />
-      <h2>Design</h2>
       <DesignSection />
     </>
   );
