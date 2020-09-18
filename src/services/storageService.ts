b1b512697b1b3df5698a95a80bc1f033537b565a
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-18 10:02:01 -0400

    get files from root storage instead of subfolders

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -1,16 +1,17 @@
 import { storageRef } from "index";
 import { DesignWork, DevelopmentWork } from "models";
 
+async function getFileURL(filename: string): Promise<string | undefined> {
+  return storageRef.child(filename).getDownloadURL() || undefined;
+}
+
 export async function getDevelopmentThumbnailURL(
   work: DevelopmentWork,
 ): Promise<string | undefined> {
-  return work.thumbnail
-    ? storageRef.child(`development/${work.thumbnail}`).getDownloadURL()
-    : undefined;
+  return getFileURL(work.thumbnail);
 }
 
 export async function getDesignThumbnailURL(work: DesignWork): Promise<string | undefined> {
-  if (work.thumbnail)
-    return storageRef.child(`design/${work.thumbnail}`).getDownloadURL() || undefined;
-  return storageRef.child(`design/${work.files[0]}`).getDownloadURL() || undefined;
+  if (work.thumbnail) return getFileURL(work.thumbnail);
+  return getFileURL(work.files[0]);
 }
