2bd0c489761c6685233db6cf61ff745624ee42dd
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-18 11:16:36 -0400

    add work headings with links to jump to sections

--- src/components/pages/WorkPage/WorkPage.tsx
+++ src/components/pages/WorkPage/WorkPage.tsx
@@ -4,9 +4,29 @@ import React from "react";
 export function WorkPage(): JSX.Element {
   return (
     <>
+      <WorkHeading heading="Development" id="development" jumpTitle="Design" jumpId="design" />
       <DevelopmentSection />
       <hr />
+      <WorkHeading heading="Design" id="design" jumpTitle="Development" jumpId="development" />
       <DesignSection />
     </>
+  );
+}
+
+interface WorkHeadingProps {
+  heading: string;
+  id: string;
+  jumpTitle: string;
+  jumpId: string;
+}
+
+function WorkHeading({ heading, id, jumpTitle, jumpId }: WorkHeadingProps): JSX.Element {
+  return (
+    <div className="d-flex justify-content-between align-items-center mb-3" id={id}>
+      <h2>{heading}</h2>
+      <a href={`#${jumpId}`} className="caption">
+        Jump to {jumpTitle}
+      </a>
+    </div>
   );
 }
