e5dc04dac2a886e9bd0311d4461195ca32b89303
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-25 09:50:01 -0400

    add under construction page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,17 +2,26 @@ import { Navigation, WorkPage } from "components";
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { UnderConstructionPage } from "./pages/UnderConstructionPage";
 
 export function App(): JSX.Element {
+  const underConstruction = true;
+
   return (
     <Router>
       <Container>
-        <Navigation />
-        <Switch>
-          <Route exact path="/work">
-            <WorkPage />
-          </Route>
-        </Switch>
+        {underConstruction ? (
+          <UnderConstructionPage />
+        ) : (
+          <>
+            <Navigation />
+            <Switch>
+              <Route exact path="/work">
+                <WorkPage />
+              </Route>
+            </Switch>
+          </>
+        )}
       </Container>
     </Router>
   );
---
+++ src/components/pages/UnderConstructionPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+export function UnderConstructionPage(): JSX.Element {
+  return (
+    <Row className="h-100">
+      <Col className="d-flex flex-column align-items-center justify-content-center">
+        <h1 className="mb-3">Stay tuned!</h1>
+        <p className="text-center">
+          This website is currently under construction. In the meantime, feel free to check out my{" "}
+          <a href="https://github.com/jonathanchaffer" target="blank">
+            GitHub profile
+          </a>
+          !
+        </p>
+      </Col>
+    </Row>
+  );
+}
