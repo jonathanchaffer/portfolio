5175e5878a8a513912820dcc7d37576fa4dc8a13
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-25 17:19:26 -0400

    update homepage content; add CTA buttons

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
-import profile from "assets/profile.jpg";
+import profile from "assets/profile-512.png";
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Col, Row } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./HomePage.scss";
 
 export function HomePage(): JSX.Element {
+  const history = useHistory();
   return (
     <div className="home-page">
       <Row>
@@ -13,12 +14,20 @@ export function HomePage(): JSX.Element {
         </Col>
         <Col xs={8} md={6} className="d-flex align-items-center">
           <div>
-            <h1 className="mb-3">Hi, I&apos;m Jon!</h1>
+            <h1>Hi, I&apos;m Jon!</h1>
             <p>
-              I&apos;m a software developer and graphic designer based in Grand Rapids, Michigan.
-              Feel free to check out my <Link to="/work">work</Link> or{" "}
-              <Link to="/contact">get in touch</Link>!
+              I&apos;m a software developer and graphic designer based in Grand Rapids, Michigan. I
+              have skills in various areas, but am especially interested in front-end development
+              and UI/UX design.
             </p>
+            <div className="buttons">
+              <Button variant="primary" onClick={() => history.push("/work")}>
+                View Portfolio
+              </Button>
+              <Button variant="outline-secondary" onClick={() => history.push("/contact")}>
+                Get in Touch
+              </Button>
+            </div>
           </div>
         </Col>
       </Row>
