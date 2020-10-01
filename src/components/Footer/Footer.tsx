6818003927f70d6f0d6cea18af01b3db8d057494
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-01 11:50:30 -0400

    add stackoverflow link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,11 +9,15 @@ export function Footer(): JSX.Element {
   return (
     <Navbar className="footer">
       <div>
-        <span className="caption">Copyright © {year} Jonathan Chaffer</span>
+        <span className="d-none d-sm-block caption">Copyright © {year} Jonathan Chaffer</span>
+        <span className="d-block d-sm-none caption">© {year} Jonathan Chaffer</span>
       </div>
       <Nav>
         <a href="https://github.com/jonathanchaffer" target="blank">
           <i className="fab fa-github" />
+        </a>
+        <a href="https://stackoverflow.com/users/3273115/jonchaf" target="blank">
+          <i className="fab fa-stack-overflow" />
         </a>
         <a href="https://www.linkedin.com/in/jonathan-chaffer/" target="blank">
           <i className="fab fa-linkedin" />
