09726e825743c2446619839a7d94aa2afc01ef82
jonathanchaffer <jonathanchaffer@gmail.com>
2020-11-22 13:50:00 -0500

    add EditModal to index.ts

--- src/components/reusables/index.ts
+++ src/components/reusables/index.ts
@@ -1,5 +1,6 @@
 export * from "./AsyncComponent";
 export * from "./AsyncImage";
+export * from "./EditModal";
 export * from "./ErrorModal";
 export * from "./ImagePreviewModal";
 export * from "./InfoModal";
